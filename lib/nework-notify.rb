40c0d5843d4738132051c6d4e114badb9e2c4005
Mahito Ogura <earthdragon77@gmail.com>
2021-01-21 16:54:58 +0900

    Apply post timing to change
    - Change default post timeing (30 min to 60 min)
    - Apply rubocop comments
    - Add version file

--- lib/nework-notify.rb
+++ lib/nework-notify.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'slack-ruby-client'
-require "net/http"
+require 'net/http'
 require 'json'
 require 'uri'
 require 'time'
@@ -11,6 +13,7 @@ NEWORK_WORKSPACE  = ENV['NEWORK_WORKSPACE']
 TOKEN_API_KEY     = ENV['TOKEN_API_KEY']
 REFRESH_TOKEN     = ENV['REFRESH_TOKEN']
 SLACK_CHANNEL     = ENV['SLACK_CHANNEL']
+UPDATE_MINUTES    = ENV['UPDATE_MINUTES'] || 60
 
 def refresh_token
   uri = URI.parse(TOKEN_REFRESH_URL + TOKEN_API_KEY)
@@ -22,16 +25,16 @@ def refresh_token
   response = Net::HTTP.post_form(uri, body)
   data = JSON.parse(response.body)
 
-  return data['id_token']
+  data['id_token']
 end
 
 Slack::Web::Client.configure do |config|
   config.token = ENV['SLACK_API_TOKEN']
   raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
 
-  STDOUT.sync = true
+  $stdout.sync = true
 
-  config.logger = Logger.new(STDOUT)
+  config.logger = Logger.new($stdout)
   config.logger.level = Logger::INFO
 end
 
@@ -41,36 +44,38 @@ ts    = 0
 
 begin
   loop do
-    uri = URI.parse(ENDPOINT + NEWORK_WORKSPACE + '/rooms')
+    uri = URI.parse("#{ENDPOINT}#{NEWORK_WORKSPACE}/rooms")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
 
-    headers = {'authorization' => "Bearer #{token}"}
+    headers = { 'authorization' => "Bearer #{token}" }
     response = http.get(uri.path, headers)
 
-    raise Net::HTTPError.new("Tokenの認証切れ", 401) if response.code == '401'
+    raise Net::HTTPError.new('Tokenの認証切れ', 401) if response.code == '401'
+
     rooms = JSON.parse(response.body)
 
     message = ''
     rooms.each do |room|
       next if room['members'].empty?
+
       message += "#{room['name']}: #{room['members'].size} 人"
-      message += "（+ #{room['listeners'].size} 人）" if room['listeners'].size != 0
+      message += "（+ #{room['listeners'].size} 人）" unless room['listeners'].empty?
       message += "\n"
     end
 
     if message == ''
       message = 'NeWorkのRoomには誰もいないよ〜'
-      if ts == 0
+      if ts.zero?
         result = slack.chat_postMessage(channel: SLACK_CHANNEL, text: message)
         ts = result['ts']
       else
         slack.chat_update(channel: SLACK_CHANNEL, text: message, ts: ts)
       end
     else
-      message =  "NeWorkの現在の状況\n" + message
+      message = "NeWorkの現在の状況\n#{message}"
       durartion = (Time.now - ts.to_i).to_i
-      if durartion > 1800
+      if durartion > UPDATE_MINUTES * 60
         result = slack.chat_postMessage(channel: SLACK_CHANNEL, text: message)
         ts = result['ts']
       else
@@ -81,6 +86,7 @@ begin
     sleep 60
   end
 rescue Net::HTTPError => e
+  puts e
   token = refresh_token
   retry
 end
