291332ff6b076a38a805e04b768bed9b6209f2f0
Mahito Ogura <earthdragon77@gmail.com>
2021-04-23 10:04:07 +0900

    fix: Change post timeing when there is nodody in NeWork's Workspace

--- lib/slack_client.rb
+++ lib/slack_client.rb
@@ -12,14 +12,24 @@ module NeWorkNotify
 
     def post(message)
       begin
-        durartion = (Time.now - @ts.to_i).to_i
-        if durartion > @update * 60
+        if expired?
           @client.chat_delete(channel: @channel, ts: @ts) unless @ts.to_i.zero?
           result = @client.chat_postMessage(channel: @channel, text: message)
           @ts = result['ts']
         else
           @client.chat_update(channel: @channel, text: message, ts: @ts)
         end
+      rescue Slack::Web::Api::Errors::SlackError => e
+        p e
+        puts e.backtrace
+        sleep 300
+        retry
+      end
+    end
+
+    def post_nobody(message)
+      begin
+        @client.chat_update(channel: @channel, text: message, ts: @ts) unless expired?
       rescue Slack::Web::Api::Errors::SlackError => e
         p e
         puts e.backtrace
@@ -39,6 +49,11 @@ module NeWorkNotify
         config.logger.level = Logger::INFO
       end
       Slack::Web::Client.new
+    end
+
+    def expired?
+      durartion = (Time.now - @ts.to_i).to_i
+      durartion > @update * 60
     end
   end
 end
